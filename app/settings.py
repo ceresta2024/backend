ecc81ef2c86fee0e2fc4d17b55cd06813aa501ff
win <win>
2024-02-20 01:02:18 +0800

    add docker file

--- app/settings.py
+++ app/settings.py
@@ -7,14 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_url():
-    driver = os.getenv("DB_TYPE")
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASS")
-    server = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    db = os.getenv("DB_NAME")
-    return f"{driver}://{user}:{password}@{server}:{port}/{db}"
+class Config:
+    DATABASE_URL = os.environ.get(
+        "DATABASE_URL", "postgresql://user:password@localhost:port/dbname"
+    )
 
 
-DATABASE_URL = get_url()
+settings = Config()
