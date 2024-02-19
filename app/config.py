eade9e6d136af4838450a97d7c16679c48eddf4d
win <win>
2024-02-20 01:04:11 +0800

    update config file

--- app/config.py
+++ app/config.py
@@ -12,5 +12,10 @@ class Config:
         "DATABASE_URL", "postgresql://user:password@localhost:port/dbname"
     )
 
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get(
+        "CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0"
+    )
+
 
 settings = Config()
--- app/models/base.py
+++ app/models/base.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.settings import DATABASE_URL
+from app.config import settings
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
