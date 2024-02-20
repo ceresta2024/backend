67be658b3838f7ebf7de94e012667473c607dbdd
win <win>
2024-02-20 12:26:03 +0800

    update user model

--- app/models/user.py
+++ app/models/user.py
@@ -9,9 +9,10 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, unique=True, index=True)
+    user_name = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     game_money = Column(Integer)
     status = Column(Integer)
-    created = Column(DateTime, default=datetime.utcnow)
+    created = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated = Column(DateTime, default=datetime.utcnow, nullable=False)
