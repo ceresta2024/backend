a5a2f398220fd466d646e4dd15815be63e91d672
win <win>
2024-02-20 15:54:34 +0800

    create user_item_log model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base
 from .user import User
 from .item import Item
+from .user_item_log import UserItemLog
---
+++ app/models/user_item_log.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, ForeignKey, orm
+
+from .base import Base
+
+
+class UserItemLog(Base):
+    __tablename__ = "user_item_log"
+
+    id = Column(Integer, primary_key=True, index=True)
+    picked_date = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    # User relationship
+    user_id: str = Column(Integer, ForeignKey("user.id"))
+    user = orm.relationship("Package")
+
+    # Item relationship
+    item_id: str = Column(Integer, ForeignKey("item.id"))
+    item = orm.relationship("Package")
