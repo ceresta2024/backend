6ee55296109717295a3aa58645390f97adb501bb
win <win>
2024-02-20 12:06:09 +0800

    add user model

--- app/models/user.py
+++ app/models/user.py
@@ -6,9 +6,12 @@ from .base import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    user_name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
+    password = Column(String)
+    game_money = Column(Integer)
+    status = Column(Integer)
     created = Column(DateTime, default=datetime.utcnow)
