e83f18030d0c5ef603049730ef490193f725d2da
win <win>
2024-02-22 02:06:00 +0800

    fix postgres url issue

--- app/config.py
+++ app/config.py
@@ -11,6 +11,8 @@ class Config:
     DATABASE_URL = os.environ.get(
         "DATABASE_URL", "postgresql://user:password@localhost:port/dbname"
     )
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     CELERY_RESULT_BACKEND = os.environ.get(
