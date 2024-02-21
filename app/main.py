d5403de2e8e53c7908648221fe571382689c0c4a
ceresta2024 <ceresta2024@outlook.com>
2024-02-20 16:10:56 -0800

    add route path prototypes

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ from app.routes import user
 
 app = FastAPI()
 
-# Include your API routers
-app.include_router(user.router, prefix="/users", tags=["users"])
+# Include specific API routers
+app.include_router(user.router, prefix="/user", tags=["user"])
+app.include_router(user.router, prefix="/maze", tags=["maze"])
+app.include_router(user.router, prefix="/shop", tags=["shop"])
 
 
 # Root endpoint
