a2869469586fdd1e20b2c4ae4524386effcb0df0
ceresta2024 <ceresta2024@outlook.com>
2024-02-20 16:14:03 -0800

    fix route path

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@
 
 from fastapi import FastAPI
 from app.config import settings
-from app.routes import user
+from app.routes import user, maze, shop
 
 
 app = FastAPI()
 
 # Include specific API routers
 app.include_router(user.router, prefix="/user", tags=["user"])
-app.include_router(user.router, prefix="/maze", tags=["maze"])
-app.include_router(user.router, prefix="/shop", tags=["shop"])
+app.include_router(maze.router, prefix="/maze", tags=["maze"])
+app.include_router(shop.router, prefix="/shop", tags=["shop"])
 
 
 # Root endpoint
