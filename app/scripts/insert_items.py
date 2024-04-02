e3371b3aea86df8d8db7e82172237d42ff3f7f94
ceresta2024 <ceresta2024@outlook.com>
2024-04-02 10:13:46 +0800

    update logic to populate item

--- app/scripts/insert_items.py
+++ app/scripts/insert_items.py
@@ -8,6 +8,13 @@ FILE_PATH = "app/scripts/items.xlsx"
 def populate_item_data():
     df = pd.read_excel(FILE_PATH)
 
+    # remove all rows that status is on working
+    df = df.drop(df[df.Status == 'Working on it'].index)
+
+    # remove unmatched columns to db
+    for column in ["Item Reference", "Status"]:
+        df.drop(column, inplace=True, axis="columns")
+
     df.columns = df.columns.str.strip()
     print(df.columns)
 
