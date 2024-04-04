80566bab5a24c39bd18205ebf03c7459b9bbabef
ceresta2024 <ceresta2024@outlook.com>
2024-04-04 09:38:55 +0800

    update store list

--- app/controllers/shop.py
+++ app/controllers/shop.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from app.models.item import Item
 from app.models.shop import Shop
-
-# from app.schemas.shop import ItemCreate
+from app.schemas.shop import StoreList
+from app.utils.common import get_list_of_dict
 
 
 class ShopController:
@@ -16,5 +17,11 @@ class ShopController:
     def get_items(self) -> list[Item]:
         return self.session.query(Item).all()
 
-    def get_store_list(self) -> list[Shop]:
-        return self.session.query(Shop).all()
+    def get_store_list(self) -> list:
+        data = (
+            self.session.query(Shop.id, Item.id, Item.name, Shop.price, Shop.quantity)
+            .join(Shop, Shop.item_id == Item.id)
+            .filter(Shop.quantity > 0)
+            .all()
+        )
+        return get_list_of_dict(StoreList.__fields__.keys(), data)
