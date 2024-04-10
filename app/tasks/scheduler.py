db53125640905dcff402028a70282d4e3d4d8a95
ceresta2024 <ceresta2024@outlook.com>
2024-04-10 11:33:43 +0800

    add background task

--- app/tasks/scheduler.py
+++ app/tasks/scheduler.py
@@ -1,3 +1,4 @@
+import schedule
 import time
 
 from datetime import datetime
@@ -14,3 +15,19 @@ def get_maze_opening_time():
 def create_task(task_type):
     time.sleep(int(task_type) * 10)
     return True
+
+
+class BackgroundTasks:
+    def __init__(self) -> None:
+        self.init_schedule_jobs()
+
+    def check_live(self):
+        print("Server is live")
+
+    def init_schedule_jobs(self):
+        schedule.every(1).minutes.do(self.check_live)
+
+    def run_schedule_jobs(self):
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
